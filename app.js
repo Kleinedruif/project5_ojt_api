143d98ec5845fe26851a14824a747e6804244214
DnJealt <jlm.vantartwijk@student.avans.nl>
2016-04-21 13:43:06 +0200

    Base, we can get to work now

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var exphbs = require('express-handlebars')
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -11,8 +12,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'main'}));
+app.set('view engine', 'hbs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
