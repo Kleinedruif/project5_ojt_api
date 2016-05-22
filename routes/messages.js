40229003100a7e53c62ed075e44711de6779cd0f
Timo Hoff <timohoff@live.nl>
2016-05-22 18:12:02 +0200

    GET /user/messages/:id

--- routes/messages.js
+++ routes/messages.js
@@ -6,24 +6,53 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',/* authenticate, */ function(req, res, next) {   
- 
-  
- 
+	var db = req.app.locals.db;
 });
 
-router.post('/:id', /*authenticate, */ function(req, res){
+router.get('/:id', function(req, res, next){
+	var db = req.app.locals.db;
+
+	var query = db('message as m').select('m.*')
+                                .where('m.receiver_guid', req.params.id);
+
+  query.then(function(messages){
+    res.json(messages);
+  });
+});
+
+router.post('/:id', function(req, res, next){
+  var db = req.app.locals.db;
+
+  console.log(req.body);
+  res.json(req.body);
+  /*db('users').insert({
+    email: user.email, 
+    hash: user.hash, 
+    salt: user.salt, 
+    name: user.name.first+' '+user.name.last, 
+    authToken: user.authToken
+  }).then(function(inserts) {
+    console.log('new message saved');
+    res.status(200).json(inserts);//user.toObject({ virtuals: true }));
+  })
+  .catch(function(error) {
+    console.error(error);
+  });*/
+});
+
+/*router.post('/:id', /.*authenticate, *./ function(req, res){
    var db = req.app.locals.db;
   
   // The person who sends the message
-  // var sendFrom = req.user.id //or something like that
+  // var sendFrom = req.user.id, or something like that
   var sendFrom = '1';
   
   //var sendTo = req.params.id;
   //TODO: get the user corresponding to this email
   
-  var sendTo = '4';
+  var sendTo = '1';
   
-  var id = uuid.v4();  
+  //var id = uuid.v4();  
   var date = new Date();
   var messageTitle = req.body.title;
   var messageBody = req.body.body;
@@ -32,7 +61,6 @@ router.post('/:id', /*authenticate, */ function(req, res){
     return res.json({ error: "Please fill in everything." });
   } 
   
-   
   db('message').insert({
     guid: id,
     sender_guid: sendFrom,
@@ -48,7 +76,10 @@ router.post('/:id', /*authenticate, */ function(req, res){
   }).catch(function(error){
     console.log(error);
   });
-   /*
+
+
+
+   /.*
   db('message_user').insert({
     'receiver_guid': sendTo,
     'message_guid': id,
@@ -59,21 +90,11 @@ router.post('/:id', /*authenticate, */ function(req, res){
     console.log();
     console.log(inserts);
   })
-  */
- 
-  
-  
-  
-  
-  
-});
+  *./
+});*/
 
-
-
-router.get('/:id', function(req, res, next) {
-  
+/*router.get('/:id', function(req, res, next) {
   var db = req.app.locals.db;
-  
   var query = db('team').where({'guid': req.params.id});
   
   if(req.query.status) {
@@ -81,14 +102,10 @@ router.get('/:id', function(req, res, next) {
   }
   
   query.then(function(user) {
-    
     var pers = user[0];
-   
     res.json(pers);
-    
   });
-  
-});
+});*/
 
 
 module.exports = router;
