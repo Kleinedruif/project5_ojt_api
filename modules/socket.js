e3c3411ab6c86523d4bae0ca3874ee939c89a337
Kleinedruif <mike_hoskamp@hotmail.com>
2016-05-25 19:11:56 +0200

    server now checks if incomming ip matches with ip in config

--- modules/socket.js
+++ modules/socket.js
@@ -11,12 +11,18 @@ module.exports = function(io) {
     io.set('authorization', function(handshake, callback){      
         // Retrive private key
         var key = ursa.createPrivateKey(fs.readFileSync('./private_key.pem'));
+
+        var webserver_ip = process.env.webserver_ip || webserverConfig.webserver_ip;
+
+        console.log(handshake.connection._peername.address);
+        //console.log(socket.request.connection.remoteAddress);
+        //console.log(socket.request.connection._peername.address);
         
         // Descerypt incomming token
         var clientToken = key.decrypt(handshake._query.token, 'base64', 'utf8');
         
         // Check if token and domain is valid
-        if (clientToken == webserverConfig.webserver_token){
+        if (clientToken == webserverConfig.webserver_token && handshake.connection._peername.address.indexOf(webserver_ip) >= 0){
             // Set connection is true and move on to the connection part
             callback(null, true);
         } else {
