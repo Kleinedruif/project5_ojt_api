87f831e262d8c712476b38307e9b51601c36516d
Kleinedruif <mike_hoskamp@hotmail.com>
2016-05-31 13:37:17 +0200

    Although when i looked in the app ranking?type=team was used so i placed it back but i did remove gender type

--- routes/ranking.js
+++ routes/ranking.js
@@ -1,23 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {  
-    var db = req.app.locals.db;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  var db = req.app.locals.db;
+  
+  var type = req.query.type;
+  var orderBy = (req.query.order ? req.query.order : "desc");
+  var query;
+  
+  if(type) {  
+      if(type == "participants") {                                                  
+        query = db('participant as p').select("p.first_name", "p.last_name", "p.gender", "p.team_guid", 't.name', "p.shirt", "p.guid", "p.team_guid")
+                                                    .leftJoin("team as t", "t.guid", "p.team_guid")
+                                                    .leftJoin('participant_has_activity as pha', 'p.guid', 'pha.participant_guid')
+                                                    .sum('pha.score as score')
+                                                    .where('p.status', 'active')
+                                                    .where('t.status', 'active')
+                                                    .groupBy('p.guid')
+                                                    .orderBy('pha.score', orderBy);
+      } else if(type == "team") {       
+        query = db("team as t").select("t.guid as team_guid", "t.name as team_name")
+                              .innerJoin("participant as p", "t.guid", "p.team_guid")
+                              .innerJoin("participant_has_activity as pha", "p.guid", "pha.participant_guid")
+                              .sum("pha.score as score")
+                              .groupBy("t.guid")
+                              .orderBy('pha.score', orderBy);      
         
-    var orderBy = (req.query.order ? req.query.order : "desc");
-                                                 
-    query = db('participant as p').select("p.first_name", "p.last_name", "p.gender", "p.team_guid", 't.name', "p.shirt", "p.guid", "p.team_guid")
-                                .leftJoin("team as t", "t.guid", "p.team_guid")
-                                .leftJoin('participant_has_activity as pha', 'p.guid', 'pha.participant_guid')
-                                .sum('pha.score as score')
-                                .where('p.status', 'active')
-                                .where('t.status', 'active')
-                                .groupBy('p.guid')
-                                .orderBy('pha.score', orderBy);
-    
-    query.then(function(score) {
+      } else {
+        return res.json({'message': 'type is not applicable'});
+      }    
+      
+      query.then(function(score) {
         res.json(score);
-    })
+      });   
+  } else {
+      return res.json({'message': 'type not set'});
+  }
+
 });
 
 module.exports = router;
