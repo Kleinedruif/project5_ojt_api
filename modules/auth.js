3585d90bf7e1032a0e72711f6824ec272a17d39c
Milan Damen <github@darvit.nl>
2016-06-01 10:36:14 +0200

    Fixed authToken checks

--- modules/auth.js
+++ modules/auth.js
@@ -5,11 +5,18 @@ var config = require('../config/auth.json');
 module.exports = {
     // The user must be logged in to access this route.
     requireLoggedIn: function(req, res, next) {
-        if (req.session.authenticated) {
-            return next();
+        if (!req.body.authToken) {
+            return res.status(401).json({message: 'U moet ingelogd zijn om deze pagina te bezoeken.'});
         }
-
-        res.status(401).json({message: 'U moet ingelogd zijn om deze pagina te bezoeken.'});
+        
+        db('user').where('authToken', req.body.authToken).then(function(user) {
+            return next();
+        })
+        .catch(function(error) {
+            return res.status(500).json({ message: error });
+        });
+        
+        return res.status(401).json({message: 'U moet ingelogd zijn om deze pagina te bezoeken.'});
     },
     
     requireRole: function(role) {
@@ -57,20 +64,6 @@ module.exports = {
                         var authToken = crypto.randomBytes(64).toString('hex');
                         
                         db('user').where('email', email).update('authToken', authToken).then(function(inserts) {
-                            req.session.authenticated = true;
-                            req.session.authToken = authToken;
-                            req.session.user = {
-                                // User data
-                                guid: user.guid,
-                                team_guid: user.team_guid,
-                                status: user.status,
-                                email: user.email,
-                                first_name: user.first_name,
-                                last_name: user.last_name,
-                                role_guid: user.role_guid,
-                                role_name: user.name             // Role name
-                            };
-                            
                             return res.status(200).json({
                                 message: "OK",
                                 auth_token: authToken,
@@ -99,6 +92,13 @@ module.exports = {
     
     // Logs the user out.
     logout: function(req) {
-        req.session.authenticated = false;
+        db('user').where('email', email).update('authToken', '').then(function(inserts) {
+            return res.status(200).json({
+                message: "OK"
+            });
+        })
+        .catch(function(error) {
+            return res.status(500).json({ message: error });
+        });
     }
 };
