b04c241680c851aabacd71fe6c1b5450df3c8957
DnJealt <jlm.vantartwijk@student.avans.nl>
2016-06-09 12:18:23 +0200

    trying to fetch ALLEEZZZ from a single event

--- routes/events.js
+++ routes/events.js
@@ -18,12 +18,34 @@ router.get('/', function (req, res, next) {
     }   
     query.then(function(result){
         return res.json(result);
-    }); 
-    
-    
-    
-    
+    });    
  });
+ 
+ router.get('/:id/days', function(req, res, next){
+     var db = req.app.locals.db;     
+     var id = req.params.id;
+     var query = db('day');  
+       
+     if(id){
+         query.where({'event_guid': id}).then(function(result){
+             if(result){
+                 console.log(result);
+                 res.json(result);
+             }
+             else{
+                 res.status(404);
+             }
+         });
+     }
+     else{
+         res.status(404);
+     }
+ });
+ 
+ // Geef mij allezzzz bij dit event.
+ router.get('/:id/', function(req, res, next){
+     
+ })
 
 
 
